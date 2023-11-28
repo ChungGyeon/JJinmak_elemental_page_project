a6e1bbef96f23e213c03f9928a8f0b89f06771cc
Ganoim <iamgwanho@gmail.com>
2023-11-28 22:28:42 +0900

    11/28 commit

---
+++ src/main/java/dto/member.java
@@ -0,0 +1,48 @@
+package dto;
+
+public class member {
+	private String mid;
+	private String mpw;
+	private String mname;
+	private String mphone;
+	private String memail;
+	private String mprofile;
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getMpw() {
+		return mpw;
+	}
+	public void setMpw(String mpw) {
+		this.mpw = mpw;
+	}
+	public String getMname() {
+		return mname;
+	}
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+	public String getMphone() {
+		return mphone;
+	}
+	public void setMphone(String mphone) {
+		this.mphone = mphone;
+	}
+	public String getMemail() {
+		return memail;
+	}
+	public void setMemail(String memail) {
+		this.memail = memail;
+	}
+	public String getMprofile() {
+		return mprofile;
+	}
+	public void setMprofile(String mprofile) {
+		this.mprofile = mprofile;
+	}
+	
+
+}
